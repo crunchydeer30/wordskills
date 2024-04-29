bc18761307f22a2c96abd88d159dc8455f7b0790
pc <crunchydeer30@gmail.com>
2024-04-29 12:20:23 +0300

    api-file-sharing: fix ApiRequest to send correct status code upon validation failure

--- api-file-sharing/app/Http/Requests/ApiRequest.php
+++ api-file-sharing/app/Http/Requests/ApiRequest.php
@@ -5,14 +5,15 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\JsonResponse;
 
 class ApiRequest extends FormRequest
 {
-  public function failedValidation(Validator $validator)
+  protected function failedValidation(Validator $validator)
   {
     throw new HttpResponseException(response()->json([
       'success' => false,
-      'messages' => $validator->errors()
-    ]));
+      'message' => $validator->errors()
+    ], 422));
   }
 }
