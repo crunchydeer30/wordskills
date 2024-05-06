7ed0608684b6c68aeceebec53ef28c22f1a1c4da
pc <crunchydeer30@gmail.com>
2024-05-06 20:28:24 +0300

    api-flight-booking: create Airport and City models

--- api-flight-booking/database/seeders/DatabaseSeeder.php
+++ api-flight-booking/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Airport;
+use App\Models\City;
 use App\Models\User;
+use Database\Factories\CityFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,5 +17,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        City::factory(10)->create();
+        Airport::factory(10)->create();
+
+        Airport::factory()->createMany(
+            [
+                [
+                    'city_id' => CityFactory::new()->create([
+                        'name' => 'Moscow',
+                    ])->id,
+                    'name' => 'Sheremetyevo',
+                    'iata' => 'SVO'
+                ],
+                [
+                    'city_id' => CityFactory::new()->create([
+                        'name' => 'Kazan',
+                    ])->id,
+                    'name' => 'Kazan',
+                    'iata' => 'KZN'
+                ],
+
+            ]
+        );
     }
 }
