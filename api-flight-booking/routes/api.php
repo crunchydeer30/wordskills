f735abe058d199f8ade68c175632d123ba4bdfd7
pc <crunchydeer30@gmail.com>
2024-05-06 20:47:53 +0300

    api-flight-booking: implement aiports endpoint

--- api-flight-booking/routes/api.php
+++ api-flight-booking/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AirportsController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
 });
+
+Route::resource('airport', AirportsController::class)->only(['index']);
