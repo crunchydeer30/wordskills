9be5367799d31b6df7fb2e5c4878ce3115c783de
pc <crunchydeer30@gmail.com>
2024-05-08 10:20:16 +0300

    api-photo-sharing: implement auth

--- api-photo-sharing/app/Models/User.php
+++ api-photo-sharing/app/Models/User.php
@@ -6,10 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    public $timestamps = null;
 
     /**
      * The attributes that are mass assignable.
@@ -17,8 +20,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'first_name',
+        'surname',
+        'phone',
         'password',
     ];
 
@@ -29,7 +33,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
