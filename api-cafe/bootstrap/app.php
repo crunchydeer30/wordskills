466534a17f26efc2224fd0dca40708fe1e201f79
pc <crunchydeer30@gmail.com>
2024-05-13 06:19:48 +0300

    api-cafe: implement exceptions

--- api-cafe/bootstrap/app.php
+++ api-cafe/bootstrap/app.php
@@ -3,17 +3,57 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            return response()->json([
+                'error' => [
+                    'code' => 404,
+                    'message' => 'Not Found'
+                ]
+            ], 404);
+        });
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'error' => [
+                    'code' => 403,
+                    'message' => 'Login Failed'
+                ]
+            ], 403);
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
+            return response()->json([
+                'error' => [
+                    'code' => 403,
+                    'message' => 'Forbidden for you'
+                ]
+            ], 403);
+        });
+
+        $exceptions->render(function (ValidationException $exception, Request $request) {
+            return response()->json([
+                'error' => [
+                    'code' => 422,
+                    'message' => 'Validation error',
+                    'errors' => $exception->errors(),
+                ]
+            ], 422);
+        });
     })->create();
